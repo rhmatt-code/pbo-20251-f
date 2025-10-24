65daebefd17dffd92b4864895096f609464df4f7
rhmatt-code <rahmat.pwpb@gmail.com>
2025-10-24 19:03:04 +0700

    Tugas Pertemuan 3

---
+++ LuasLingkaran.php
@@ -0,0 +1,15 @@
+<?php
+
+class LuasLingkaran{
+
+    public const phi = 3.14;
+    public int $jari;
+    
+}
+
+$lingkaran = new LuasLingkaran();
+$lingkaran-> jari = 5;
+
+$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
+
+echo "Luas Lingkaran: ", $rumus;
